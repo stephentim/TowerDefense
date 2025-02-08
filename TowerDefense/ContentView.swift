e59a41a0279ff9aeca5200b7ab5070492f2aded2
Stephen Tim <571268@qq.com>
2025-02-09 00:22:31 +0800

    Initial Commit

---
+++ TowerDefense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TowerDefense
+//
+//  Created by Stephen Tim on 2025/2/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TowerDefense/TowerDefenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TowerDefenseApp.swift
+//  TowerDefense
+//
+//  Created by Stephen Tim on 2025/2/9.
+//
+
+import SwiftUI
+
+@main
+struct TowerDefenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
